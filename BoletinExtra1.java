f640b7e644220b2eeb6cd1beb48f81206b835fec
PabloVilaM <91198045+PabloVilaM@users.noreply.github.com>
2021-10-26 10:32:11 +0200

    Add files via upload

---
+++ BoletinExtra1.java
@@ -0,0 +1,21 @@
+package boletinextra1;
+
+public class BoletinExtra1 {
+
+ 
+    public static void main(String[] args) {
+        float nota1 = 10;
+        float nota2 = 9; 
+       float media = (nota1+nota2)/2;
+        
+        //Ejercicio 1
+        System.out.println("Pablo Vila Mauricio \n 20/02/2002");
+        
+        //Ejercicio 2
+        System.out.println("Programacion \n Primera nota: " + nota1 + "\n Segunda nota: " + nota2 + "\n Nota total: " + media);
+        
+        //Ejercicio 3
+        System.out.println("Pantalones rebajados: " + (90-(90*25)/100));
+    }
+    
+}
